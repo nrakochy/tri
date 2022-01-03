b4edcad720e3bd67ffecdcff1785666e4b2a3589
Nick Rakochy <nrakochy@anduril.com>
2022-01-03 09:53:15 -0600

    Init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/nrakochy/tri
+
+go 1.16
+
+require github.com/spf13/cobra v1.3.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2022 Nick Rakochy
+
+*/
+package main
+
+import "github.com/nrakochy/tri/cmd"
+
+func main() {
+	cmd.Execute()
+}
